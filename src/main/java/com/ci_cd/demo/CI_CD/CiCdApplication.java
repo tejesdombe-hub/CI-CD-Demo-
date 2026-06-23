b73a106368d8903bbbab940165e2e3ef6722c8b7
Tejes Dombe <tejes.dombe@successive.tech>
2026-06-23 12:31:31 +0530

    Adding the Swagar Dependency

--- src/main/java/com/ci_cd/demo/CI_CD/CiCdApplication.java
+++ src/main/java/com/ci_cd/demo/CI_CD/CiCdApplication.java
@@ -3,6 +3,10 @@ package com.ci_cd.demo.CI_CD;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+
+@OpenAPIDefinition(info = @Info(title = "CI/CD API", version = "1.0", description = "API for CI/CD"))
 @SpringBootApplication
 public class CiCdApplication {
 
